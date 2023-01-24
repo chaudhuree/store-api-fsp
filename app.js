cae122bfdaf126289e79c7d36ce71b404652f1d1
chaudhuree <chaudhuree00@gmail.com>
2023-01-25 03:59:10 +0600

    4️⃣ base router setup and use it in app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect');
-
+const productsRouter = require('./routes/products');
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
@@ -17,6 +17,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
+app.use('/api/v1/products', productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
